7c9425c7194c9a0ccc3c50430319f6beae192efe
Andrea Giammarchi <andrea.giammarchi@gmail.com>
2020-12-09 19:02:07 +0100

    added Secretly for the Web

--- README.md
+++ README.md
@@ -2,9 +2,10 @@
 
 [![Build Status](https://travis-ci.com/WebReflection/secretly.svg?branch=master)](https://travis-ci.com/WebReflection/secretly) [![Coverage Status](https://coveralls.io/repos/github/WebReflection/secretly/badge.svg?branch=master)](https://coveralls.io/github/WebReflection/secretly?branch=master)
 
-A basic class to encrypt and decrypt
+A basic class to encrypt and decrypt.
 
 ```js
+// NodeJS
 import Secretly from 'secretly';
 // or const Secretly = require('secretly');
 
@@ -19,4 +20,30 @@ const pvt = new Secretly(
 
 const encrypted = pvt.encrypt('any text');
 const decrypted = pvt.decrypt(encrypted);
+```
+
+### Secretly Web
+
+It's the same class with the following differences:
+
+  * both `encrypt` and `decrypt` are *asynchronous*
+  * the `salt` parameter is by default the `location.href`
+  * there is a static helper to make salts less greedy, which is `Secretly.PATH`, returning the current domain + path without search and hashes, like `location.href` would do
+  * the module is *not compatible* with browsers that don't support *ES2015* and it won't be published as such
+
+```js
+// Web
+import Secretly from '//unpkg.com/secretly/web?module';
+
+const pvt = new Secretly(
+  'my-password',
+  // optional second argument as salt
+  Secretly.PATH,
+  // optional third argument to avoid
+  // using a random iv (when same output per input is needed)
+  false
+);
+
+const encrypted = await pvt.encrypt('any text');
+const decrypted = await pvt.decrypt(encrypted);
 ```
---
+++ cjs/web.js
@@ -0,0 +1,128 @@
+'use strict';
+// https://github.com/WebReflection/caller-of#readme
+const callerOf = method => method.call.bind(method);
+const proto = ({prototype}) => prototype;
+
+// secured globals
+const {
+  crypto, location, parseInt,
+  Array,
+  Number,
+  Object,
+  String,
+  TextDecoder, TextEncoder,
+  Uint8Array,
+  WeakMap
+} = self;
+
+// secured WeakMap methods
+const wm = new WeakMap;
+const get = callerOf(wm.get);
+const set = callerOf(wm.set);
+
+// secured Array methods
+let tmp = proto(Array);
+const join = callerOf(tmp.join);
+const map = callerOf(tmp.map);
+const push = callerOf(tmp.push);
+
+// secured TextDecoder / TextEncoder methods
+const decode = callerOf(proto(TextDecoder).decode);
+const encode = callerOf(proto(TextEncoder).encode);
+
+// secured crypto methods
+const getRandomValues = callerOf(crypto.getRandomValues);
+
+// secured crypto.subtle methods
+const {subtle} = crypto;
+const decrypt = callerOf(subtle.decrypt);
+const encrypt = callerOf(subtle.encrypt);
+const deriveKey = callerOf(subtle.deriveKey);
+const importKey = callerOf(subtle.importKey);
+
+// secured String methods
+tmp = proto(String);
+const slice = callerOf(tmp.slice);
+const substr = callerOf(tmp.substr);
+
+// secured Number methods
+const toString = callerOf(proto(Number).toString);
+
+// secured freeze
+const {freeze} = Object;
+
+const asHEX = i => slice('0' + toString(i, 16), -2);
+
+const genEncryptionKey = async (password, salt) => {
+  const name = 'PBKDF2';
+  const derived = {name: 'AES-GCM', length: 256};
+  const encoded = encode(new TextEncoder, password);
+  const key = await importKey(subtle, 'raw', encoded, {name}, false, ['deriveKey']);
+  return deriveKey(
+    subtle,
+    {
+      name,
+      hash: 'SHA-256',
+      salt: encode(new TextEncoder, salt),
+      iterations: 1000
+    },
+    key,
+    derived,
+    false,
+    ['encrypt', 'decrypt']
+  );
+};
+
+const hex = buff => join(map(new Uint8Array(buff), asHEX), '');
+
+const xeh = str => {
+  const bytes = [];
+  for (let i = 0, {length} = str; i < length; i += 2)
+    push(bytes, parseInt(substr(str, i, 2), 16));
+  return new Uint8Array(bytes);
+};
+
+class Secretly {
+
+  static get PATH() {
+    const {protocol, host, pathname} = location;
+    return protocol + '//' + host + pathname;
+  }
+
+  constructor(
+    password,
+    salt = location.href,
+    random = true
+  ) {
+    const pwd = String(password || '');
+    if (!pwd || !salt)
+      throw new TypeError(`invalid password`);
+    const buffer = new Uint8Array(16);
+    set(wm, this, {
+      key: genEncryptionKey(pwd, salt),
+      info: {
+        name: 'AES-GCM',
+        length: 256,
+        iv: random ?
+              getRandomValues(crypto, buffer) :
+              buffer
+      }
+    });
+  }
+
+  async decrypt(encrypted) {
+    const {key, info} = get(wm, this);
+    const descrypted = await decrypt(subtle, info, await key, xeh(encrypted));
+    return decode(new TextDecoder, descrypted);
+  }
+
+  async encrypt(decrypted) {
+    const {key, info} = get(wm, this);
+    const encrypted = encode(new TextEncoder, decrypted);
+    return hex(await encrypt(subtle, info, await key, encrypted));
+  }
+}
+
+freeze(proto(Secretly));
+
+module.exports = freeze(Secretly);
